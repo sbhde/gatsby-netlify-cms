e2dbb3434a08f2c58c0e62a04a956d11b5a27698
koolmilk <sbosse1409@googlemail.com>
2021-08-04 12:44:02 +0200

    Changed TestBlock Component

--- src/components/TestBlock.js
+++ src/components/TestBlock.js
@@ -3,7 +3,18 @@ import PropTypes from 'prop-types'
 
 const TestBlock = ({ data }) => (
     <div className="test-blockdata">
-        {data}
+        <div className="test-blockdata-child">
+            <h1>{data}</h1>
+            <p>{data}</p>
+        </div>
+        <div className="test-blockdata-child">
+            <h1>{data}</h1>
+            <p>{data}</p>
+        </div>
+        <div className="test-blockdata-child">
+            <h1>{data}</h1>
+            <p>{data}</p>
+        </div>
     </div>
   )
 
